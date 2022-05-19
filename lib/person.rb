94380401b87ed602333083644bd133010b16c49b
Marcos Martinez <mmtz2108@gmail.com>
2022-05-19 16:27:00 -0500

    commit tree 93f27c5fa5af15ef047c095ec5704976d65ec7ab

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,49 @@
-# your code goes here
+require "pry"
+
+class Person
+attr_reader :name, :happiness, :hygiene
+attr_accessor :bank_account 
+
+def initialize(name, bank_account, happiness, hygiene)
+    @name = name
+    @bank_account = 25
+    @happiness = 8
+    @hygiene = 8
+end
+
+def happiness=(value)
+    @happiness =
+    if value > 10
+        10
+    elsif < 0
+        0
+    else
+        value
+    end
+end
+
+def hygiene=(value)
+    @hygiene = 
+    if value >= 10
+        10
+    elsif <= 0
+       0
+        else
+            value
+            end
+        end
+
+def clean?
+    self.hygiene > 7
+end
+
+def happy?
+    self.happiness > 7
+end
+
+def get_paid(amount)
+    self.bank_account += amount
+    "all about the benjamins"
+end
+      
+end
